4a34bd6ce42da0718e9c1dad27ca030858284e2a
sandeepyarabal <sandeep.yarabal@gmail.com>
2020-06-11 17:02:16 -0400

    Add files via upload

---
+++ mazeCreationBasicMazeGame.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+
+using namespace std;
+
+enum  Direction { North, South, East, West };
+class MapSite {
+public:
+    virtual void Enter() = 0;
+};
+class Room : public MapSite {
+public:
+    Room(int roomNo)
+    {
+        _roomNumber = roomNo;
+    };
+
+    void SetSide(int Dir, MapSite* wr)
+    {
+        _sides[Dir] = wr;
+    };
+    void Enter()
+    {};
+private:
+    MapSite* _sides[4];
+    int _roomNumber;
+};
+class Wall : public MapSite {
+public:
+    Wall() {};
+    void Enter() {};
+};
+class Door : public MapSite {
+public:
+    Door(Room* = 0, Room* = 0) {};
+    void Enter() {};
+
+private:
+    Room* _room1;
+    Room* _room2;
+    bool _isOpen;
+};
+
+class Maze {
+public:
+    Maze() {};
+    void AddRoom(Room*) {};
+    Room* RoomNo(int) {};
+private:
+    // ...
+};
+
+class MazeGame {
+public:
+    MazeGame() {};
+    Maze* CreateMaze();
+
+private:
+
+};
+Maze* MazeGame::CreateMaze()
+{
+    Maze* aMaze = new Maze;
+    Room* r1 = new Room(1);
+    Room* r2 = new Room(2);
+    Door* theDoor = new Door(r1, r2);
+    aMaze->AddRoom(r1);
+    aMaze->AddRoom(r2);
+    r1->SetSide(North, new Wall);
+    r1->SetSide(East, theDoor);
+    r1->SetSide(South, new Wall);
+    r1->SetSide(West, new Wall);
+    r2->SetSide(North, new Wall);
+    r2->SetSide(East, new Wall);
+    r2->SetSide(South, new Wall);
+    r2->SetSide(West, theDoor);
+    return aMaze;
+}
+int main()
+{
+    MazeGame game;
+    game.CreateMaze();
+    cout << "hello world";
+    return 0;
+}
+
