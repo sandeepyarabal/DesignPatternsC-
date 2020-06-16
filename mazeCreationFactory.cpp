ac7d4928569cdc7a279eee0fac5242f0277e4363
sandeepyarabal <sandeep.yarabal@gmail.com>
2020-06-16 09:04:05 -0400

    Update mazeCreationFactory.cpp

--- mazeCreationFactory.cpp
+++ mazeCreationFactory.cpp
@@ -49,16 +49,6 @@ public:
 private:
     // ...
 };
-class MazeBuilder {
-public:
-    virtual void BuildMaze() { }
-    virtual void BuildRoom(int room) { }
-    virtual void BuildDoor(int roomFrom, int roomTo) { }
-    virtual Maze* GetMaze() { return 0; }
-//protected:
-    MazeBuilder() {};
-};
-
 
 class MazeGame {
 public:
