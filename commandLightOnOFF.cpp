d0510f5ceed2451555135db7856062c3225d045b
sandeepyarabal <sandeep.yarabal@gmail.com>
2020-09-10 19:09:32 -0400

    Create commandLightOnOFF.cpp

---
+++ commandLightOnOFF.cpp
@@ -0,0 +1,96 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+example taken from bogotobogo
+*******************************************************************************/
+
+#include <vector>
+#include <iostream> 
+#include <algorithm> 
+#include <functional> 
+using namespace std;
+
+// Command Interface
+class Command
+{
+public:
+	virtual void execute() = 0;
+};
+ 
+// Receiver Class
+class Light 
+{
+public:
+	void on() {
+		cout << "The light is on\n";
+	}
+	void off() {
+		cout << "The light is off\n";
+	}
+}; 
+
+// Command for turning on the light
+class LightOnCommand : public Command 
+{
+public:
+        LightOnCommand(Light *light) : mLight(light) {}
+	void execute(){
+		mLight->on();
+	}
+private:
+	Light *mLight;
+};
+ 
+// Command for turning off the light
+class LightOffCommand : public Command 
+{
+public:
+        LightOffCommand(Light *light) : mLight(light) {}
+	void execute(){
+		mLight->off();
+	}
+private:
+	Light *mLight;
+};
+
+// Invoker 
+// Stores the ConcreteCommand object 
+class RemoteControl 
+{
+public:
+	void setCommand(Command *cmd) {
+		mCmd = cmd;
+	}
+
+	void buttonPressed() {
+		mCmd->execute();
+	} 
+private:
+	Command *mCmd;
+};
+ 
+// The client
+int main() 
+{
+	// Receiver 
+	Light *light = new Light;
+
+	// concrete Command objects 
+	LightOnCommand *lightOn = new LightOnCommand(light);
+	LightOffCommand *lightOff = new LightOffCommand(light);
+
+	// invoker objects
+	RemoteControl *control = new RemoteControl;
+
+	// execute
+	control->setCommand(lightOn);
+	control->buttonPressed();
+	control->setCommand(lightOff);
+	control->buttonPressed();
+
+	delete light, lightOn, lightOff, control;
+
+	return 0;
+}
